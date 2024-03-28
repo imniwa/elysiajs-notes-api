d5057814aae2b0eb83af176984ad8d08e1665c2c
kur <kurniawanronaldi@gmail.com>
2024-03-29 01:29:01 +0700

    create notes-app

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,56 @@
 import { Elysia } from "elysia";
+import { router } from "./router";
+import { logger } from "@bogeychan/elysia-logger";
+import { cors } from "@elysiajs/cors";
+import swagger from "@elysiajs/swagger";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(
+    logger({
+      level: "info",
+      crlf: true,
+    })
+  )
+  .use(
+    swagger({
+      provider: "swagger-ui",
+      path: "/docs",
+      exclude: ["/docs", "/docs/json", "/"],
+      documentation: {
+        info: {
+          title: "Notes API",
+          description: "API for notes",
+          version: "0.1.0",
+          contact: {
+            email: "niwaisme@proton.me",
+            name: "imniwa",
+          },
+        },
+        tags: [
+          { name: "auth", description: "Auth operations" },
+          { name: "users", description: "Users operations" },
+          { name: "notes", description: "Notes operations" },
+        ],
+        components: {
+          securitySchemes: {
+            bearerAuth: {
+              type: "http",
+              scheme: "bearer",
+              bearerFormat: "JWT",
+            },
+          },
+        },
+        security: [
+          {
+            bearerAuth: [],
+          },
+        ],
+      },
+    })
+  )
+  .use(cors())
+  .use(router)
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
